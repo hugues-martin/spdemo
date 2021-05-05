79dd33d2b526fe8a0e4779187b387a1932858dd6
huguesm <huguesmartin@gmail.com>
2021-05-05 14:25:40 +0200

    Stupid change

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into users(userId, userName, userEmail, address) values (1000, 'Bobr', 'bob@gmail.com', 'Paris');
+insert into users(userId, userName, userEmail, address) values (1000, 'Bobs', 'bob@gmail.com', 'Paris');
 insert into users(userId, userName, userEmail, address) values (1001, 'Pat', 'pat@gmail.com', 'London');
 insert into users(userId, userName, userEmail, address) values (1002, 'Rupert', 'rupert@gmail.com', 'Chicago');
