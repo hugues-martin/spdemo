689e4837ec004f9de246d06fdaa0d76c38abcc74
huguesm <huguesmartin@gmail.com>
2021-05-05 12:16:19 +0200

    Commit stuff

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into users(userId, userName, userEmail, address) values (1000, 'Dinesh', 'dinesh@gmail.com', 'Delhi');
-insert into users(userId, userName, userEmail, address) values (1001, 'Kumar', 'kumar@gmail.com', 'Greater Noida');
-insert into users(userId, userName, userEmail, address) values (1002, 'Rajput', 'rajput@gmail.com', 'Noida');
+insert into users(userId, userName, userEmail, address) values (1000, 'Bob', 'bob@gmail.com', 'Paris');
+insert into users(userId, userName, userEmail, address) values (1001, 'Pat', 'pat@gmail.com', 'London');
+insert into users(userId, userName, userEmail, address) values (1002, 'Rupert', 'rupert@gmail.com', 'Chicago');
